a360df86c479f687801fcacd39ae7b629d0e0629
Arshdeep <arshsingh3223@gmail.com>
2021-06-10 15:47:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+  function saturdayFun( b="roller-skate") {
+    return(`This Saturday, I want to ${b}!`);
+
+  }
+  //onsole.log(saturdayFun())
+
+function mondayWork(b="go to the office"){
+    return(`This Monday, I will ${b}.` );
+}
+
+function wrapAdjective(string = "*") { 
+    return function innerFunction (g = "special") { 
+      return (`You are ${string}${g}${string}!`) ; 
+    }
+
+}
